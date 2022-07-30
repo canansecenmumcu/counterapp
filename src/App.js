fd58234e56b88a8a1f89f4f98bec571444ab79a3
canansecenmumcu <canansecenmumcu@gmail.com>
2022-07-30 12:56:35 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+function App(){
+  return(
+    <div>
+      <h1>Counter App</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App;
